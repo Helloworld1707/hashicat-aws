3937011f04a1cae1c4cb7be20b24d882bd972d21
Hello World <helloworld1707@gmail.com>
2021-06-04 00:45:11 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -8,8 +8,6 @@ module "s3_bucket" {
   source  = "app.terraform.io/Thiyagu_Jayaraju-training/s3-bucket/aws"
   version = "2.2.0"
 
-  
-  bucket = "Gaurav1111xxxxx11111"
   bucket_prefix = "thiyagu-jayaraju"
   
 }
