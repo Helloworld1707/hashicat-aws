2606beff419a07f0f0f0e603abc2a793a56f7b16
Hello World <helloworld1707@gmail.com>
2021-06-04 00:41:01 +0000

    Added s3 bucket module

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -1,8 +1,6 @@
 //--------------------------------------------------------------------
 // Variables
-variable "s3_bucket_acceleration_status" {}
-variable "s3_bucket_policy" {}
-variable "s3_bucket_request_payer" {}
+
 
 //--------------------------------------------------------------------
 // Modules
@@ -10,9 +8,8 @@ module "s3_bucket" {
   source  = "app.terraform.io/Thiyagu_Jayaraju-training/s3-bucket/aws"
   version = "2.2.0"
 
-  acceleration_status = "${var.s3_bucket_acceleration_status}"
+  
   bucket = "Gaurav"
   bucket_prefix = "thiyagu-jayaraju"
-  policy = "${var.s3_bucket_policy}"
-  request_payer = "${var.s3_bucket_request_payer}"
+  
 }
